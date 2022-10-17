745747e9583938ff1b04fe21169c28c6cf89fb34
yorkyu <yorkyu@tencent.com>
2022-10-17 15:33:37 +0800

    feat: some change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Webify, Yorkyu Learn React
         </a>
       </header>
     </div>
